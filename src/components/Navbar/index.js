e2449a24e2f0ad63bd4fbc625122bd076c69bf59
ashleyerffmeyer <ashley.erffmeyer@gmail.com>
2019-10-15 18:29:17 -0500

    updated Navbar index.js file

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ function Navbar(props) {
 
         <nav className="navbar navbar-default navbar-fixed-top">
             <ul>
-                <li className="itemLeft" href="/">Sanrio Clicky Game</li>
+                <li className="itemLeft" href="https://whispering-brook-22023.herokuapp.com">Sanrio Clicky Game</li>
                 <li className="itemCenter">{props.message}</li>
                 <li className="itemRight">Score: {props.score} | Top Score: {props.topScore}</li>
             </ul>
